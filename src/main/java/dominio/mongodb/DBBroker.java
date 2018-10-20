8b6c0fa972c5c8ca12a2abb01ee1408805f5f348
Lydia Prado <Lydia.prado@alu.uclm.es>
2018-10-20 21:16:18 +0100

    DBBroker creado y dependencias de mongodb añadidas a travis

---
+++ src/main/java/dominio/mongodb/DBBroker.java
@@ -0,0 +1,22 @@
+package dominio.mongodb;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+public class DBBroker {
+
+	MongoClient cliente = null;
+	MongoDatabase bd = null;
+
+	public DBBroker() {
+
+		if(cliente != null) {
+			String acceso = "mongodb://usuarioGeneral:usuarioGeneral2018@ds233763.mlab.com:33763/avengerslive";
+			MongoClientURI uri = new MongoClientURI(acceso);
+			cliente = new MongoClient(uri);
+		}	
+			bd = cliente.getDatabase("avengerslive");
+	}
+
+}
