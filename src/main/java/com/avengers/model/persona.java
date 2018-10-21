80691fba87e7793a9d787dc2fd3099daf3c38b3d
carlossRA <carlos.rodriguez24@alu.uclm.es>
2018-10-21 18:36:33 +0200

    cambios

---
+++ src/main/java/com/avengers/model/persona.java
@@ -0,0 +1,21 @@
+package com.avengerslive.model;
+
+public class persona {
+     private  String nombre;
+     private String apellido;
+   
+    public String getNombre() {
+    	return this.nombre;
+    }
+    public void SetNombre(String nombre) {
+    	this.nombre=nombre;
+    }
+    public String getApellido() 
+    {
+        return this.apellido;	
+    }
+    public void SetApellido(String apellido) {
+    	this.apellido=apellido;
+    }
+    
+}
