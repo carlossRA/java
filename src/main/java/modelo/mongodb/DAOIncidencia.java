1ee608f11d75ae575c2bcf4c0ad654afe83fb810
Lydia Prado <Lydia.prado@alu.uclm.es>
2018-11-24 22:28:49 +0100

    Primera versión de los filtros de las incidencias (Back-end)

--- src/main/java/modelo/mongodb/DAOIncidencia.java
+++ src/main/java/modelo/mongodb/DAOIncidencia.java
@@ -14,11 +14,11 @@ public class DAOIncidencia {
 	private MongoCollection<Document> coleccion;
 
 	public DAOIncidencia() {
-		
+
 		db = new DBBroker();
 		coleccion = db.devolverColeccion("Incidencias");
 	}
-	
+
 	public List<Document> consultarIncidenciasPropias(String idEmpleado) {
 		List<Document> incidencias = new ArrayList<Document>();
 		Document documento = new Document();
@@ -28,23 +28,22 @@ public class DAOIncidencia {
 			if(documento.get("idEmpleado").toString().equalsIgnoreCase(idEmpleado))
 				incidencias.add(documento);
 		}
-		
+
 		return incidencias;
-		
+
 	}
-	
+
 	public List<Document> consultarIncidenciasGestor(){
 		List<Document> incidencias = new ArrayList<Document>();
 		Document documento = new Document();
 		MongoCursor<Document> elementos = db.documentosEnColeccion(coleccion);
 		while(elementos.hasNext()) {
 			documento = elementos.next();
-			if(!documento.get("mensaje").toString().equalsIgnoreCase("Resuelta"))
-				incidencias.add(documento);
+			incidencias.add(documento);
 		}
 		return incidencias;		
 	}
-	
+
 
 	public void cambiarMensaje(String idEmpleado, String comentario, String nuevoMensaje) {
 		Document documento = new Document();
@@ -55,9 +54,21 @@ public class DAOIncidencia {
 		cambio.put("mensaje", nuevoMensaje);
 		documento.put("$set", cambio);
 		db.actualizarDocumento(coleccion, filtro, documento);
-		
+
 	}
-	
+
+	public List<Document> filtrar(String tipo, String valor, List<Document> totalIncidencias){
+		List<Document> incidencias = new ArrayList<Document>();
+		Document documento = new Document();
+		for(int i = 0; i < totalIncidencias.size(); i++) {
+			documento = totalIncidencias.get(i);
+			if(documento.get(tipo).toString().equalsIgnoreCase(valor))
+				incidencias.add(documento);
+		}
+
+		return incidencias;
+	}
+
 	//////////////////////////ANA///////////////////////////
 	public void registrarIncidencia(Incidencia incidencia) {
 		Document documento = new Document();
@@ -69,7 +80,7 @@ public class DAOIncidencia {
 				.append("fechaFin", incidencia.getFechaFin())
 				.append("comentario", incidencia.getComentario()));
 	}
-	
+
 	public boolean comprobarIncidencia(String idEmpleado, String tipo, String mensaje) {
 		Document documento = new Document();
 		MongoCursor<Document> elementos = db.documentosEnColeccion(coleccion);
@@ -85,5 +96,5 @@ public class DAOIncidencia {
 	}
 	////////////////////////////////////////////////////////////////
 
-	
+
 }
